3c971efebb138d215709535b7e7b84fe71d4ee41
songmilee <lsm941010@gmail.com>
2020-02-04 22:34:11 +0900

    구현:2577

---
+++ bkj.java/src/bkj/java/P2577.java
@@ -0,0 +1,31 @@
+package bkj.java;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P2577 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Long multi = 1L;
+		
+		for(int i = 0; i < 3; i++) {
+			int val = Integer.parseInt(br.readLine());
+			multi *= val;
+		}
+		
+		int[] cnt = new int[10];
+		
+		String result = multi.toString();
+		for(int i = 0; i < result.length() ; i++) {
+			cnt[result.charAt(i) - '0']++;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(cnt[i]);
+		}
+		
+		br.close();
+	}
+
+}
